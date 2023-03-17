318ae4d3de648a6db5a82cda5331404667e5c65e
LunashaGit <lunamuylkens@gmail.com>
2023-03-17 12:23:24 +0100

    Push Correct RefreshDatabase & Correct Functions in test

--- tests/Feature/PeopleTest.php
+++ tests/Feature/PeopleTest.php
@@ -6,9 +6,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Models\People;
 class PeopleTest extends TestCase
 {
     use WithoutMiddleware;
+
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
@@ -36,13 +39,43 @@ class PeopleTest extends TestCase
 
     public function test_that_people_create_page_is_working_with_post() : void
     {
+        
+        $people = People::factory()->make();
+
         $response = $this->post('/people/create', [
-            'name' => 'John',
-            'last_name' => 'Doe',
+            'name' =>  $people->name,   
+            'last_name' => $people->last_name,
         ]);
+
 
         $response->assertStatus(302);
 
+    }
+
+    public function test_that_people_edit_page_is_working_with_put() : void
+    {
+        
+        $people = People::factory()->make();
+
+        $people->name = 'John';
+        $people->last_name = 'Doe';
+
+        $people->save();
+
+        $this->assertDatabaseHas('people', [
+            'name' => $people->name,
+            'last_name' => $people->last_name,
+        ]);
+
+    }
+
+    public function test_that_people_delete_page_is_working(): void
+    {
+        $people = People::factory()->make();
+
+        $people->delete();
+        
+        $this->assertModelMissing($people);
     }
 
     public function test_that_people_filter_is_working(): void
